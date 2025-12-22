8fd8bd68638683003fcb242be71d05a13f703b59
Your Name <olamilekansunday445@gmail.com>
2025-12-22 17:16:17 +0000

    remove case study section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Footer } from '@/components/footer';
 import { ParallaxBackground } from '@/components/parallax-background';
 import { HeroSection } from '@/app/sections/hero';
 import { ProjectsSection } from '@/app/sections/projects';
-import { CaseStudySection } from '@/app/sections/case-study';
 import { AboutSection } from '@/app/sections/about';
 import { ContactSection } from '@/app/sections/contact';
 import { EducationSection } from '@/app/sections/education';
@@ -12,14 +11,13 @@ import { ScrollToTopButton } from '@/components/scroll-to-top-button';
 
 export default function Home() {
   return (
-          <div className="relative flex flex-col min-h-screen overflow-x-hidden">
+          <div className="relative flex flex-col min-h-screen">
 
       <ParallaxBackground />
       <Header />
       <main className="flex-grow z-10">
         <HeroSection />
         <ProjectsSection />
-        <CaseStudySection />
         <AboutSection />
         <EducationSection />
         <ContactSection />
